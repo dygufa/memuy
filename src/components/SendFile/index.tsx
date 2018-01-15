2e5105a3969d4e928d0ba66b2a1901a869500f99
Rodrigo Araujo <dygufa@gmail.com>
2018-01-15 01:11:58 -0300

    adding input file element on send file component

--- src/components/SendFile/index.tsx
+++ src/components/SendFile/index.tsx
@@ -1,18 +1,32 @@
 import * as React from "react";
+import { ChangeEvent, FormEvent } from "react";
 
 const s = require("./style.scss");
 
 interface ISendFileProps {
     color: "white" | "blue";
+    onFileInputChange: (files: FileList) => void;
 };
 
 interface ISendFileState {};
 
 class SendFile extends React.Component<ISendFileProps, ISendFileState> {
+    private fileInputRef: HTMLInputElement;
+
+    private onClick = () => {
+        this.fileInputRef.click();
+    }
+
     public render(): JSX.Element {
         return (
-            <div className={`${s.cloud} ${s[this.props.color]}`}>
-                <span>Arraste ou clique para fazer upload</span>
+            <div onClick={this.onClick} className={`${s.cloud} ${s[this.props.color]}`}>
+                <span>Drag or click to upload a file</span>
+                <input 
+                    className={s.fileInput} 
+                    type="file" 
+                    onChange={(e) => this.props.onFileInputChange(e.target.files!)}
+                    ref={(ref) => this.fileInputRef = ref!}
+                />
             </div>
         );
     }
